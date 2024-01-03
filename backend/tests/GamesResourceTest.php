167dfc57236463673b6d889f73d1b1b2b5ef4432
ForendUwU <nikitaperepelov040604@gmail.com>
2024-01-03 18:02:18 +0300

    Tests for all games endpoints

--- backend/tests/GamesResourceTest.php
+++ backend/tests/GamesResourceTest.php
@@ -3,7 +3,6 @@
 namespace App\Tests;
 
 use App\Entity\Game;
-use App\Entity\User;
 use App\Factory\GameFactory;
 use App\Factory\UserFactory;
 use Doctrine\ORM\EntityManager;
@@ -49,7 +48,7 @@ class GamesResourceTest extends WebTestCase
     public function testGetGamesCollection(): void
     {
         GameFactory::createMany(5);
-        $this->client->request(
+        $this->client->jsonRequest(
             'GET',
             'https://localhost/api/games'
         );
@@ -69,7 +68,7 @@ class GamesResourceTest extends WebTestCase
         ]);
     }
 
-    public function testCreateNewGameSuccess(): void
+    public function testPostNewGameSuccess(): void
     {
         $this->authorize();
         $this->client->jsonRequest(
@@ -98,7 +97,7 @@ class GamesResourceTest extends WebTestCase
         ]);
     }
 
-    public function testCreateNewGameUnauthorized(): void
+    public function testPostNewGameUnauthorized(): void
     {
         $this->client->jsonRequest(
             'POST',
@@ -123,10 +122,9 @@ class GamesResourceTest extends WebTestCase
         );
     }
 
-    public function testCreateNewGameInvalidInput(): void
+    public function testPostNewGameInvalidInput(): void
     {
         $this->authorize();
-        dump($this->manager->getRepository(User::class)->findAll());
         $this->client->jsonRequest(
             'POST',
             'https://localhost/api/games',
@@ -141,7 +139,160 @@ class GamesResourceTest extends WebTestCase
         $response = $this->client->getResponse();
         $decodedResponse = json_decode($response->getContent(), true);
 
-        dump($decodedResponse);
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $this->assertArrayHasKey('detail', $decodedResponse);
+        $this->assertEquals(
+            'The type of the "title" attribute must be "string", "integer" given.',
+            $decodedResponse['detail']
+        );
+    }
+
+    public function testPostNewUnprocessableEntity(): void
+    {
+        $this->authorize();
+        $this->client->jsonRequest(
+            'POST',
+            'https://localhost/api/games',
+            [
+                'title' => 'Game0',
+                'description' => 'test_description',
+                'price' => 99,
+                'purchasedGames' => []
+            ]
+        );
+
+        $this->client->jsonRequest(
+            'POST',
+            'https://localhost/api/games',
+            [
+                'title' => 'Game0',
+                'description' => 'test_description',
+                'price' => 99,
+                'purchasedGames' => []
+            ]
+        );
+
+        $response = $this->client->getResponse();
+        $decodedResponse = json_decode($response->getContent(), true);
+
+        $this->assertEquals(422, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $this->assertArrayHasKey('violations', $decodedResponse);
+        $this->assertArrayHasKey('message', $decodedResponse['violations'][0]);
+        $this->assertEquals(
+            'Game with this title already exists',
+            $decodedResponse['violations'][0]['message']
+        );
+    }
+
+    public function testGetGameSuccess(): void
+    {
+        GameFactory::createMany(5);
+        $this->client->jsonRequest(
+            'GET',
+            'https://localhost/api/games/1'
+        );
+
+        $response = $this->client->getResponse();
+        $decodedResponse = json_decode($response->getContent(), true);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $this->assertSame(array_keys($decodedResponse), [
+            'id',
+            'title',
+            'description',
+            'price',
+            'publishedAt',
+            'purchasedGames'
+        ]);
+    }
+
+    public function testGetGameResourceNotFound(): void
+    {
+        GameFactory::createMany(5);
+        $this->client->jsonRequest(
+            'GET',
+            'https://localhost/api/games/6'
+        );
+
+        $response = $this->client->getResponse();
+        $decodedResponse = json_decode($response->getContent(), true);
+
+        $this->assertEquals(404, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $this->assertEquals('Not Found', $decodedResponse['detail']);
+    }
+
+    public function testDeleteGameSuccess(): void
+    {
+        GameFactory::createMany(5);
+        $this->client->jsonRequest(
+            'DELETE',
+            'https://localhost/api/games/5'
+        );
+
+        $response = $this->client->getResponse();
+        $deletedGame = $this->manager->getRepository(Game::class)->findBy(['id' => 5]);
+
+        $this->assertEquals(204, $response->getStatusCode());
+        $this->assertEmpty($deletedGame);
+    }
+
+    public function testDeleteGameResourceNotFound(): void
+    {
+        GameFactory::createMany(5);
+        $this->client->jsonRequest(
+            'DELETE',
+            'https://localhost/api/games/6'
+        );
+
+        $response = $this->client->getResponse();
+        $deletedGame = $this->manager->getRepository(Game::class)->findBy(['id' => 5]);
+
+        $this->assertEquals(404, $response->getStatusCode());
+        $this->assertNotEmpty($deletedGame);
+    }
+
+    public function testPatchGameSuccess(): void
+    {
+        GameFactory::createMany(5);
+        $this->client->jsonRequest(
+            'PATCH',
+            'https://localhost/api/games/5',
+            [
+                'title' => 'Test_game'
+            ],
+            [
+                'CONTENT_TYPE' => 'application/merge-patch+json',
+            ]
+        );
+
+        $response = $this->client->getResponse();
+        $decodedResponse = json_decode($response->getContent(), true);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $this->assertEquals('Test_game', $decodedResponse['title']);
+    }
+
+    public function testPatchGameInvalidInput(): void
+    {
+        GameFactory::createMany(5);
+        $this->client->jsonRequest(
+            'PATCH',
+            'https://localhost/api/games/5',
+            [
+                "title" => 123,
+            ],
+            [
+                'CONTENT_TYPE' => 'application/merge-patch+json',
+            ]
+        );
+
+        $response = $this->client->getResponse();
+        $decodedResponse = json_decode($response->getContent(), true);
 
         $this->assertEquals(400, $response->getStatusCode());
         $this->assertJson($response->getContent());
@@ -152,27 +303,73 @@ class GamesResourceTest extends WebTestCase
         );
     }
 
-    public function testGetGame(): void
+    public function testPatchGameResourceNotFound(): void
     {
         GameFactory::createMany(5);
-        $gameId = $this->manager->getRepository(Game::class)->findAll()[0]->getId();
-        $this->client->request(
-            'GET',
-            'https://localhost/api/games/'.$gameId
+        $this->client->jsonRequest(
+            'PATCH',
+            'https://localhost/api/games/6',
+            [
+                'title' => 'Test_game'
+            ],
+            [
+                'CONTENT_TYPE' => 'application/merge-patch+json',
+            ]
         );
 
         $response = $this->client->getResponse();
         $decodedResponse = json_decode($response->getContent(), true);
 
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals(404, $response->getStatusCode());
         $this->assertJson($response->getContent());
-        $this->assertSame(array_keys($decodedResponse), [
-            'id',
-            'title',
-            'description',
-            'price',
-            'publishedAt',
-            'purchasedGames'
-        ]);
+        $this->assertEquals('Not Found', $decodedResponse['detail']);
+    }
+
+    public function testPatchNewUnprocessableEntity(): void
+    {
+        $this->authorize();
+        $this->client->jsonRequest(
+            'POST',
+            'https://localhost/api/games',
+            [
+                'title' => 'Game0',
+                'description' => 'test_description',
+                'price' => 99,
+                'purchasedGames' => []
+            ]
+        );
+        $this->client->jsonRequest(
+            'POST',
+            'https://localhost/api/games',
+            [
+                'title' => 'Game1',
+                'description' => 'test_description',
+                'price' => 99,
+                'purchasedGames' => []
+            ]
+        );
+
+        $this->client->jsonRequest(
+            'PATCH',
+            'https://localhost/api/games/2',
+            [
+                'title' => 'Game0',
+            ],
+            [
+                'CONTENT_TYPE' => 'application/merge-patch+json',
+            ]
+        );
+
+        $response = $this->client->getResponse();
+        $decodedResponse = json_decode($response->getContent(), true);
+
+        $this->assertEquals(422, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $this->assertArrayHasKey('violations', $decodedResponse);
+        $this->assertArrayHasKey('message', $decodedResponse['violations'][0]);
+        $this->assertEquals(
+            'Game with this title already exists',
+            $decodedResponse['violations'][0]['message']
+        );
     }
 }
