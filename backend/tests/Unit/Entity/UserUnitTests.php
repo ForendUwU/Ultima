60a6b9cb8dd46e755a0e1c2872cf508c8c4d5e03
ForendUwU <nikitaperepelov040604@gmail.com>
2024-01-24 14:42:45 +0300

    Add tests for User Entity

--- backend/tests/Unit/Entity/UserUnitTests.php
+++ backend/tests/Unit/Entity/UserUnitTests.php
@@ -16,11 +16,30 @@ class UserUnitTests extends TestCase
     {
         $testUser = new User();
         $this->assertNotNull($testUser);
-        $this->assertNotNull($testUser->getId());
         $this->assertNotNull($testUser->getCreatedAt());
         $this->assertNotNull($testUser->getBalance());
         $this->assertEquals(0, $testUser->getBalance());
         $this->assertNotNull($testUser->getPurchasedGames());
         $this->assertNotNull($testUser->getTokens());
+    }
+
+    public function testCreateNotEmptyUserEntity(): void
+    {
+        $testUser = new User();
+        $testUser->setLogin('Test Login');
+        $testUser->setRoles(['ROLE_ADMIN']);
+        $testUser->setPassword('testPassword');
+        $testUser->setNickname('Test Nickname');
+        $testUser->setFirstName('Test FirstName');
+        $testUser->setLastName('Test LastName');
+        $testUser->setEmail('test@email.com');
+
+        $this->assertEquals('Test Login', $testUser->getLogin());
+        $this->assertContains('ROLE_ADMIN', $testUser->getRoles());
+        $this->assertEquals('testPassword', $testUser->getPassword());
+        $this->assertEquals('Test Nickname', $testUser->getNickname());
+        $this->assertEquals('Test FirstName', $testUser->getFirstName());
+        $this->assertEquals('Test LastName', $testUser->getLastName());
+        $this->assertEquals('test@email.com', $testUser->getEmail());
     }
 }
